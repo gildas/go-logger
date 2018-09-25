bf9885b613c7c3dd25719b53408652336c3b7a8a
Gildas Cherruel <gildas@breizh.org>
2018-09-25 22:23:18 +0900

    Better Http Handler

--- logger.go
+++ logger.go
@@ -165,41 +165,33 @@ func (l *Logger) ToContext(parent context.Context) context.Context {
 	return context.WithValue(parent, ContextKey, l)
 }
 
-// HandlerFunc function will wrap an http handler with extra logging information
-func HandlerFunc(logger *Logger, h http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		// Get a request identifier
-		reqid := r.Header.Get("X-Line-Request-Id")
-		if len(reqid) == 0 { reqid = r.Header.Get("X-Request-Id") }
-		if len(reqid) == 0 { reqid = uuid.Must(uuid.NewV1()).String() }
-
-		reqLogger := logger.Record("topic", "route").Record("scope", r.URL.Path).Record("reqid", reqid).Child()
-		reqLogger.Infof("request start: %s %s", r.Method, html.EscapeString(r.URL.Path))
-		h.ServeHTTP(w, r.WithContext(reqLogger.(*Logger).ToContext(context.WithValue(r.Context(), "reqid", reqid))))
-		duration := time.Now().Sub(start)
-		reqLogger.Record("duration", duration.Seconds()).Infof("request finish: %s %s", r.Method, html.EscapeString(r.URL.Path))
-	})
-}
-
-// Handler function will wrap an http handler with extra logging information
-func (l *Logger) Handler() func(http.Handler) http.Handler {
+// HttpHandler function will wrap an http handler with extra logging information
+func (l *Logger) HttpHandler() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 
-			// Get a request identifier
+			// Get a request identifier and pass it to the response writer
 			reqid := r.Header.Get("X-Line-Request-Id")
 			if len(reqid) == 0 { reqid = r.Header.Get("X-Request-Id") }
 			if len(reqid) == 0 { reqid = uuid.Must(uuid.NewV1()).String() }
 			w.Header().Set("X-Request-Id", reqid)
 
-			reqLogger := l.Record("topic", "route").Record("scope", r.URL.Path).Record("reqid", reqid).Child()
-			reqLogger.Record("remote", r.RemoteAddr).Record("UserAgent", r.UserAgent()).Infof("request start: %s %s", r.Method, html.EscapeString(r.URL.Path))
-			next.ServeHTTP(w, r.WithContext(reqLogger.(*Logger).ToContext(context.WithValue(r.Context(), "reqid", reqid))))
+                        // Get a new Child logger tailored to the request
+			reqLogger := l.Record("topic", "route").Record("scope", r.URL.Path).Record("reqid", reqid).Child().(*Logger)
+			reqLogger.
+			  Record("remote", r.RemoteAddr).
+                          Record("UserAgent", r.UserAgent()).
+                          Infof("request start: %s %s", r.Method, html.EscapeString(r.URL.Path))
+
+			// Adding reqid and reqLogger to r.Context and serving the request
+			next.ServeHTTP(w, r.WithContext(reqLogger.ToContext(context.WithValue(r.Context(), "reqid", reqid))))
+
+                        // Logging the duration of the request handling
 			duration := time.Now().Sub(start)
-			reqLogger.Record("duration", duration.Seconds()).Infof("request finish: %s %s", r.Method, html.EscapeString(r.URL.Path))
+			reqLogger.
+                          Record("duration", duration.Seconds()).
+                          Infof("request finish: %s %s", r.Method, html.EscapeString(r.URL.Path))
 		})
 	}
 }
