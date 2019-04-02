9c1fa16da363abd75cad6f2c5431a8c2c0d87914
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-04-02 16:49:07 +0900

    Better version string

--- version.go
+++ version.go
@@ -1,3 +1,7 @@
 package logger
 
-const VERSION string = "0.0.1"
+// commit contains the current git commit and is set in the build.sh script
+var commit string
+
+// VERSION is the version of this application
+var VERSION = "0.0.1" + commit
