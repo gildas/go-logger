17e9ae3427c13c8b0aa8e7b36b82de5aec2f8da9
Gildas <gildas@breizh.org>
2019-11-22 19:57:55 +0900

    return errors with callstack

--- record.go
+++ record.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"encoding/json"
+	"github.com/pkg/errors"
 )
 
 // Record is the map that contains all records of a log entry
@@ -56,7 +57,7 @@ func (record Record) MarshalJSON() ([]byte, error) {
 func (record *Record) UnmarshalJSON(payload []byte) error {
 	var placeholder map[string]interface{}
 	if err := json.Unmarshal(payload, &placeholder); err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 	*record = Record(placeholder)
 	return nil
