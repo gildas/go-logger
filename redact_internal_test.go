8d69f73081ef3b29468c0678d06092c8a6b673f0
Gildas Cherruel <gildas@breizh.org>
2022-11-27 21:49:14 +0900

    cosmetics

--- redact_internal_test.go
+++ redact_internal_test.go
@@ -21,12 +21,12 @@ func (suite *InternalLoggerSuite) TestCanCreateChildLoggerWithRedactors() {
 	suite.Require().NotNil(log, "cannot create a Logger with redactors")
 	suite.Require().Len(log.redactors, 2, "The Logger should have 2 redactors")
 
-	child := log.Child(nil, nil, "mytopic", "myvalue", core.Must(NewRedactor("[A-Z]{8}")))
+	child := log.Child(nil, nil, "mylabel", "myvalue", core.Must(NewRedactor("[A-Z]{8}")))
 	suite.Require().NotNil(child, "cannot create a child Logger")
 	suite.Assert().Len(child.redactors, 3, "The Child Logger should have 3 redactors")
 	suite.Assert().Len(log.redactors, 2, "The Parent Logger should have 2 redactors")
 
-	child2 := log.Child(nil, nil, "mytopic", 3, *(core.Must(NewRedactor("[A-Z]{8}"))))
+	child2 := log.Child(nil, nil, "mylabel", 3, *(core.Must(NewRedactor("[A-Z]{8}"))))
 	suite.Require().NotNil(child2, "cannot create a child Logger")
 	suite.Assert().Len(child.redactors, 3, "The Child Logger should have 3 redactors")
 	suite.Assert().Len(log.redactors, 2, "The Parent Logger should have 2 redactors")
