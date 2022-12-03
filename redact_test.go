f97284ad08fbc9bc433716bcced56bac88a1c155
Gildas Cherruel <gildas@breizh.org>
2022-12-03 14:18:36 +0900

    Redactor helpers

--- redact_test.go
+++ redact_test.go
@@ -1,6 +1,8 @@
 package logger_test
 
 import (
+	"strings"
+
 	"github.com/gildas/go-core"
 	"github.com/gildas/go-logger"
 )
@@ -51,6 +53,69 @@ func (suite *LoggerSuite) TestCanRedactMessage() {
 	})
 }
 
+func (suite *LoggerSuite) TestShouldNotRedactMessageWithNoMatch() {
+	redactor := core.Must(logger.NewRedactor(`\+[0-9]{23}`))
+	suite.Require().NotEmpty(redactor.String())
+	output := CaptureStdout(func() {
+		log := logger.Create(
+			"test",
+			&logger.StdoutStream{Unbuffered: true},
+			redactor,
+		)
+		log.Infof("message with sensitive (+13178723000) data")
+	})
+	suite.LogLineEqual(output, map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      `message with sensitive \(\+13178723000\) data`,
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+}
+
+func (suite *LoggerSuite) TestCanRedactMessageWithSeveralRedactors() {
+	output := CaptureStdout(func() {
+		log := logger.Create(
+			"test",
+			&logger.StdoutStream{Unbuffered: true},
+			logger.VISARedactor,
+			logger.AMEXRedactor,
+		)
+		log.Infof("message with sensitive (VISA: 4977949494949497) data")
+		log.Infof("message with sensitive (AMEX: 370000000100018) data")
+	})
+	lines := strings.Split(output, "\n")[0:2]
+	suite.LogLineEqual(lines[0], map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      `message with sensitive \(VISA: REDACTED\) data`,
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+	suite.LogLineEqual(lines[1], map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      `message with sensitive \(AMEX: REDACTED\) data`,
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+}
+
 func (suite *LoggerSuite) TestCanRedactAsString() {
 	suite.Assert().Equal("", logger.Redact(nil))
 	suite.Assert().Equal("", logger.Redact(""))
@@ -63,3 +128,25 @@ func (suite *LoggerSuite) TestCanRedactAsString() {
 	suite.Assert().Equal("Name-6cea57c2fb", logger.RedactWithPrefixedHash("Name", "John Doe"))
 }
 
+func (suite *LoggerSuite) TestCanRedactAMEXCard() {
+	output := CaptureStdout(func() {
+		log := logger.Create(
+			"test",
+			&logger.StdoutStream{Unbuffered: true},
+			logger.AMEXRedactor,
+		)
+		log.Infof("message with sensitive (371234567890123) data")
+	})
+	suite.LogLineEqual(output, map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      `message with sensitive \(REDACTED\) data`,
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+}
--- redactor.go
+++ redactor.go
@@ -1,8 +1,55 @@
 package logger
 
-import "regexp"
+import (
+	"regexp"
+
+	"github.com/gildas/go-core"
+)
 
 type Redactor regexp.Regexp
+
+var (
+	// CreditCardRedactor is a Redactor that will redact Credit Card Numbers
+	CreditCardRedactor = core.Must(NewRedactor(`(4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)`))
+
+	// AMEXRedactor is a Redactor that will redact AMEX Numbers
+	AMEXRedactor = core.Must(NewRedactor(`3[47][0-9]{13}`))
+
+	// DinersClubRedactor is a Redactor that will redact Diners Club Numbers
+	DinersClubRedactor = core.Must(NewRedactor(`3(?:0[0-5]|[68][0-9])[0-9]{11}`))
+
+	// DiscoverRedactor is a Redactor that will redact Discover Numbers
+	DiscoverRedactor = core.Must(NewRedactor(`6(?:011|5[0-9]{2})[0-9]{12}`))
+
+	// JCBRedactor is a Redactor that will redact JCB Numbers
+	JCBRedactor = core.Must(NewRedactor(`(?:2131|1800|35\d{3})\d{11}`))
+
+	// MasterCardRedactor is a Redactor that will redact MasterCard Numbers
+	MasterCardRedactor = core.Must(NewRedactor(`(5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}`))
+
+	// VISARedactor is a Redactor that will redact VISA Credit Card Numbers
+	VISARedactor = core.Must(NewRedactor(`4[0-9]{12}(?:[0-9]{3})?`))
+
+	// PhoneRedactor is a Redactor that will redact Phone Numbers
+	PhoneRedactor = core.Must(NewRedactor(`(\+?[0-9]{1,3}[-. ]?[0-9]{3}[-. ]?[0-9]{3}[-. ]?[0-9]{4})|(\+?[0-9]{1,3}[-. ]?[0-9.\-]+)`))
+
+	// EmailRedactor is a Redactor that will redact Email Addresses
+	EmailRedactor = core.Must(NewRedactor(`[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,4}`))
+
+	// SSNRedactor is a Redactor that will redact Social Security Numbers
+	SSNRedactor = core.Must(NewRedactor(`\d{3}-\d{2}-\d{4}`))
+
+	// IPRedactor is a Redactor that will redact IP Addresses (v4 and v6)
+	IPRedactor = core.Must(NewRedactor(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|([0-9a-fA-F:]{2,39})`))
+	// IPV4Redactor is a Redactor that will redact IP v4 Addresses
+	IPV4Redactor = core.Must(NewRedactor(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`))
+
+	// IPV6Redactor is a Redactor that will redact IP v6 Addresses
+	IPV6Redactor = core.Must(NewRedactor(`[0-9a-fA-F:]{2,39}`))
+
+	// MACRedactor is a Redactor that will redact MAC Addresses
+	MACRedactor = core.Must(NewRedactor(`([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})`))
+)
 
 func NewRedactor(regex string) (*Redactor, error) {
 	r, err := regexp.Compile(regex)
