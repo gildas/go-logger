a47d985ea8db75502eb7f378114c334716a0383e
Gildas Cherruel <gildas@breizh.org>
2026-02-09 00:56:22 +0900

    Issue #22: Added support for obfuscation

--- obfuscator.go
+++ obfuscator.go
@@ -1,6 +1,55 @@
 package logger
 
+import (
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"regexp"
+)
+
+var unobfuscateRex = regexp.MustCompile("(.*)!ENC!:{(.*)}(.*)")
+
 // Obfuscate obfuscates the given string
 func (l *Logger) Obfuscate(value string) string {
+	if l.ObfuscationKey != nil {
+		var err error
+		var gcm cipher.AEAD
+
+		if gcm, err = cipher.NewGCM(l.ObfuscationKey); err == nil {
+			nonce := make([]byte, gcm.NonceSize())
+			if _, err = io.ReadFull(rand.Reader, nonce); err == nil {
+				return "!ENC!:{" + base64.URLEncoding.EncodeToString(gcm.Seal(nonce, nonce, []byte(value), nil)) + "}"
+			}
+		}
+		l.Child("logger", "obfuscate").Errorf("Failed to obfuscate", err)
+		return value
+	}
+	l.Child("logger", "obfuscate").Warnf("WARNING, the next string will not be obfuscated because no obfuscation key was provided")
 	return value
+}
+
+// Unobfuscate the given string
+func (l *Logger) Unobfuscate(value string) (unobfuscated string, err error) {
+	if l.ObfuscationKey != nil {
+		if components := unobfuscateRex.FindStringSubmatch(value); len(components) == 4 {
+			var decoded []byte
+
+			if decoded, err = base64.URLEncoding.DecodeString(components[2]); err == nil {
+				var gcm cipher.AEAD
+
+				if gcm, err = cipher.NewGCM(l.ObfuscationKey); err == nil {
+					var decrypted []byte
+
+					nonce := decoded[:gcm.NonceSize()]
+					decoded = decoded[gcm.NonceSize():]
+					if decrypted, err = gcm.Open(nil, nonce, decoded, nil); err == nil {
+						return components[1] + string(decrypted) + components[3], nil
+					}
+				}
+			}
+			return value, err
+		}
+	}
+	return value, nil
 }
--- obfuscator_test.go
+++ obfuscator_test.go
@@ -1,27 +1,19 @@
 package logger_test
 
 import (
-	"fmt"
+	"crypto/aes"
+	"encoding/json"
 	"reflect"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/gildas/go-logger"
 	"github.com/stretchr/testify/suite"
 )
 
 type ObfuscatorSuite struct {
-	suite.Suite
-	Name   string
-	Start  time.Time
-	Logger *logger.Logger
-}
-
-func (suite *ObfuscatorSuite) TestCanManuallyObfuscate() {
-	value := "secret stuff"
-	encrypted := suite.Logger.Obfuscate(value)
-	suite.Assert().NotEqual(value, encrypted)
+	LoggerSuite
+	Name string
 }
 
 func TestObfuscatorSuite(t *testing.T) {
@@ -29,34 +21,169 @@ func TestObfuscatorSuite(t *testing.T) {
 }
 
 func (suite *ObfuscatorSuite) SetupSuite() {
-	suite.Name = strings.TrimSuffix(reflect.TypeOf(*suite).Name(), "Suite")
-	suite.Logger = logger.Create("test",
-		&logger.FileStream{
-			Path:        fmt.Sprintf("./log/test-%s.log", strings.ToLower(suite.Name)),
-			Unbuffered:  true,
-			FilterLevel: logger.TRACE,
-		},
-	).Child("test", "test")
-	suite.Logger.Infof("Suite Start: %s %s", suite.Name, strings.Repeat("=", 80-14-len(suite.Name)))
+	suite.Name = strings.TrimSuffix(reflect.TypeOf(suite).Elem().Name(), "Suite")
 }
 
 func (suite *ObfuscatorSuite) TearDownSuite() {
 	if suite.T().Failed() {
-		suite.Logger.Warnf("At least one test failed, we are not cleaning")
 		suite.T().Log("At least one test failed, we are not cleaning")
-	} else {
-		suite.Logger.Infof("All tests succeeded, we are cleaning")
 	}
-	suite.Logger.Infof("Suite End: %s %s", suite.Name, strings.Repeat("=", 80-12-len(suite.Name)))
-	suite.Logger.Close()
 }
 
-func (suite *ObfuscatorSuite) BeforeTest(suiteName, testName string) {
-	suite.Logger.Infof("Test Start: %s %s", testName, strings.Repeat("-", 80-13-len(testName)))
-	suite.Start = time.Now()
+func (suite *ObfuscatorSuite) TestCanObfuscate() {
+	cipherBlock, err := aes.NewCipher([]byte("1234567890123456"))
+	suite.Require().NoError(err, "Failed to create cipher block")
+
+	customer := User{"12345678", "John Doe", nil}
+	output := CaptureStdout(func() {
+		log := logger.Create("test", cipherBlock, &logger.StdoutStream{Unbuffered: true})
+		log.Record("customer", customer).Infof("This should be hidden: %s and more text", log.Obfuscate("secret stuff"))
+	})
+	// suite.T().Logf("output:\n%s", output)
+	lines := strings.Split(output, "\n")
+	lines = lines[0 : len(lines)-1] // remove the last empty line
+	suite.Require().Len(lines, 1, "There should be 1 line in the log output, found %d", len(lines))
+	suite.LogLineEqual(lines[0], map[string]string{
+		"customer": `map\[id:12345678 name:REDACTED\]`,
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      "This should be hidden: !ENC!:{.*} and more text",
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+
+	log := logger.Create("test", cipherBlock, &logger.StdoutStream{Unbuffered: true})
+	var entry map[string]any
+	err = json.Unmarshal([]byte(lines[0]), &entry)
+	suite.Require().NoError(err, "Failed to unmarshal log line #0")
+	cleartext, err := log.Unobfuscate(entry["msg"].(string))
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("This should be hidden: secret stuff and more text", cleartext)
 }
 
-func (suite *ObfuscatorSuite) AfterTest(suiteName, testName string) {
-	duration := time.Since(suite.Start)
-	suite.Logger.Record("duration", duration.String()).Infof("Test End: %s %s", testName, strings.Repeat("-", 80-11-len(testName)))
+func (suite *ObfuscatorSuite) TestCanObfuscateWithoutKey() {
+	// In that case, the logger will not obfuscate anything and should add a warning in the logs
+	customer := User{"12345678", "John Doe", nil}
+	output := CaptureStdout(func() {
+		log := logger.Create("test", &logger.StdoutStream{Unbuffered: true})
+		log.Record("customer", customer).Infof("%s", log.Obfuscate("secret stuff"))
+	})
+	lines := strings.Split(output, "\n")
+	lines = lines[0 : len(lines)-1] // remove the last empty line
+	suite.Require().Len(lines, 2, "There should be 2 lines in the log output, found %d", len(lines))
+	suite.LogLineEqual(lines[0], map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "40",
+		"msg":      "WARNING, the next string will not be obfuscated because no obfuscation key was provided",
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "obfuscate",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "logger",
+		"v":        "0",
+	})
+	suite.LogLineEqual(lines[1], map[string]string{
+		"customer": `map\[id:12345678 name:REDACTED\]`,
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      "secret stuff",
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+}
+
+func (suite *ObfuscatorSuite) TestCanUnobfuscate() {
+	cipherBlock, err := aes.NewCipher([]byte("1234567890123456"))
+	suite.Require().NoError(err, "Failed to create cipher block")
+	log := logger.Create("test", cipherBlock, &logger.StdoutStream{Unbuffered: true})
+
+	obfuscated := "This should be hidden: !ENC!:{rAdNoayKlX5t8uKvmZ8FBadpq--P2bbem7Dqero33wL3gguQcHKiBA==} and more text"
+	cleartext, err := log.Unobfuscate(obfuscated)
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("This should be hidden: secret stuff and more text", cleartext)
+
+	obfuscated = "!ENC!:{rAdNoayKlX5t8uKvmZ8FBadpq--P2bbem7Dqero33wL3gguQcHKiBA==} and more text"
+	cleartext, err = log.Unobfuscate(obfuscated)
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("secret stuff and more text", cleartext)
+
+	obfuscated = "This should be hidden: !ENC!:{rAdNoayKlX5t8uKvmZ8FBadpq--P2bbem7Dqero33wL3gguQcHKiBA==}"
+	cleartext, err = log.Unobfuscate(obfuscated)
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("This should be hidden: secret stuff", cleartext)
+
+	obfuscated = "!ENC!:{rAdNoayKlX5t8uKvmZ8FBadpq--P2bbem7Dqero33wL3gguQcHKiBA==}"
+	cleartext, err = log.Unobfuscate(obfuscated)
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("secret stuff", cleartext)
+
+	obfuscated = "some text"
+	cleartext, err = log.Unobfuscate(obfuscated)
+	suite.Require().NoError(err, "Failed to unobfuscate log line #0")
+	suite.Assert().Equal("some text", cleartext)
+
+}
+
+type bogusCipherBlock struct{}
+
+func (block bogusCipherBlock) BlockSize() int          { return 0 }
+func (block bogusCipherBlock) Encrypt(dst, src []byte) {}
+func (block bogusCipherBlock) Decrypt(dst, src []byte) {}
+
+func (suite *ObfuscatorSuite) TestShouldFailObfuscateWithWrongBlockSize() {
+	output := CaptureStdout(func() {
+		log := logger.Create("test", bogusCipherBlock{}, &logger.StdoutStream{Unbuffered: true})
+		log.Infof("%s", log.Obfuscate("secret stuff"))
+	})
+	lines := strings.Split(output, "\n")
+	lines = lines[0 : len(lines)-1] // remove the last empty line
+	suite.Require().Len(lines, 2, "There should be 2 lines in the log output, found %d", len(lines))
+	suite.LogLineEqual(lines[0], map[string]string{
+		"err":      `map\[\]`,
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "50",
+		"msg":      "Failed to obfuscate. Error:",
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "obfuscate",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "logger",
+		"v":        "0",
+	})
+	suite.LogLineEqual(lines[1], map[string]string{
+		"hostname": `[a-zA-Z_0-9\-\.]+`,
+		"level":    "30",
+		"msg":      "secret stuff",
+		"name":     "test",
+		"pid":      "[0-9]+",
+		"scope":    "main",
+		"tid":      "[0-9]+",
+		"time":     `[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z`,
+		"topic":    "main",
+		"v":        "0",
+	})
+}
+
+func (suite *ObfuscatorSuite) TestShouldFailUnobfuscateWithBogusEncryptedData() {
+	log := logger.Create("test", bogusCipherBlock{}, &logger.StdoutStream{Unbuffered: true})
+	_, err := log.Unobfuscate("!ENC!:{dummyvalue}")
+	suite.Require().Error(err, "Should Fail to unobfuscate log line #0")
+}
+
+func (suite *ObfuscatorSuite) TestShouldFailUnobfuscateWithWrongBlockSize() {
+	log := logger.Create("test", bogusCipherBlock{}, &logger.StdoutStream{Unbuffered: true})
+	_, err := log.Unobfuscate("!ENC!:{K8KCYXK_vo8VkJhECFel4_qhi1EXlKk0Hd7Um2hiAeAKWkgiZWUvCA==}")
+	suite.Require().Error(err, "Should Fail to unobfuscate log line #0")
 }
